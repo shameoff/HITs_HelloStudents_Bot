2acda975c8e27ea7cf06ef84b9701675003d3aeb
shameoff <eshamov030316@gmail.com>
2022-08-26 05:10:35 +0700

    add readme, finish bot.js

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HITs_HelloStudents_Bot
 This bot was created for discord server of Higher IT School TSU for greeting new students there.
+
+## To start the bot
+You need to download nodejs and create .env in directory near to bot.js and create files welcomeTextFor1stGradeServer and welcomeTextForFreeTimeServer here. But I think it would be better to refactor a name of the Hello message text and delete ternary operator at the end of the bot.js.
+
+To run the bot with prepared script write this in the console 
+  `nodejs bot.js`
--- bot.js
+++ bot.js
@@ -1,15 +1,30 @@
+const fs = require("fs")
+
 require("dotenv").config(); //to start process from .env file
+
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, messageLink } = require('discord.js');
+const { log } = require("console");
 const token = process.env.TOKEN;
 
+const welcomeMessageFor1stGrade = fs.readFileSync("./welcomeTextFor1stGradeServer").toString();
+const welcomeMessageForFreeTime = fs.readFileSync("./welcomeTextForFreeTimeServer").toString();
+
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Bot is online!');
 });
 
+
+client.on("guildMemberAdd", async member => {
+	// let student_role = member.guild.roles.cache.find((role) => role.name === "Student")
+	// member.roles.add(student_role)
+	let welcome_channel = member.guild.channels.cache.find((channel) => channel.name === "welcome")
+	welcome_channel.send(`${member}` + (member.guild.name == "972205"? welcomeMessageFor1stGrade : welcomeMessageForFreeTime))
+	console.log("NEW USER!")
+})
 // Login to Discord with your client's token
 client.login(token);
